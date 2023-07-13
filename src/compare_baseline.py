0f920a99786faf2c1e13d62150950b4d450500ae
erik <eaa299@usask.ca>
2023-07-13 16:04:47 -0600

    f

--- src/compare_baseline.py
+++ src/compare_baseline.py
@@ -106,6 +106,71 @@ def create_fine_tune_plot_averaged_from_records(baseline, test_sets):
     plt.savefig(out_path)
 
 
+def create_fine_tune_plot_averaged_dic_from_records(baseline, test_sets):
+
+
+
+    # fig = plt.figure(figsize=(10, 10))
+    # ax = fig.add_subplot(111)
+
+    fig, axs = plt.subplots(2, 3, figsize=(18, 10))
+
+    letter_labels = ["A", "B", "C", "D", "E", "F"]
+
+    for i, test_set in enumerate(test_sets):
+        print(i)
+        # if i == 0:
+        #     continue
+        test_set_str = test_set["username"] + ":" + test_set["farm_name"] + ":" + test_set["field_name"] + ":" + test_set["mission_date"]
+    
+        record_path = os.path.join("eval_charts", "fine_tuning", "selected_first", test_set_str + "_" + baseline["model_name"] + "_results.json")
+        results = json_io.load_json(record_path)
+
+        ax = axs[i // 3, i % 3]
+        
+        ax.plot([x[0] for x in results["random_patches_second"]], [x[4] for x in results["random_patches_second"]], c=my_plot_colors[0], label="Random Region Selection", zorder=1)
+        ax.plot([x[0] for x in results["selected_patches_first"]], [x[4] for x in results["selected_patches_first"]], c=my_plot_colors[1], label="Uncertainty-Based Region Selection", zorder=1)
+    
+        for x in results["random_patches_second"]:
+            #print(x)
+            ax.scatter([x[0]] * len(x[6]), x[6], c=my_plot_colors[0], marker="o", zorder=2, alpha=0.3, s=20, edgecolors='none')
+
+        for x in results["selected_patches_first"]:
+            ax.scatter([x[0]] * len(x[6]), x[6], c=my_plot_colors[1], marker="o", zorder=2, alpha=0.3, s=20, edgecolors='none')
+
+
+        ax.axhline(y=results["pre_fine_tune_accuracy"], c="black", linestyle="dashdot", label="No Fine-Tuning")
+
+        # axs[i // 3, i % 3].legend()
+        ax.set_ylabel("Instance-Based Accuracy")
+        ax.set_xlabel("Number of Annotations Used For Fine-Tuning") #Annotations")
+
+        props = dict(edgecolor="white", facecolor="white") #boxstyle='round', facecolor='white') #, alpha=0.5)
+
+        ax.text(0.04, 0.89, letter_labels[i], transform=ax.transAxes, fontsize=24,
+                                bbox=props)
+
+        ax.set_ylim([0.7, 1])
+
+    handles, labels = axs[1, 2].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper right', fontsize=11)
+    # from matplotlib import rcParams
+    # rcParams['axes.titlepad'] = 20 
+    fig.suptitle("Targeted Annotation for Fine-Tuning: Results for Six Image Sets", size=18) #, y=1.12)
+
+
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.9) 
+    out_path = os.path.join("eval_charts", "fine_tuning", "selected_first", "averaged_global_all_results.svg")
+    out_dir = os.path.dirname(out_path)
+    os.makedirs(out_dir, exist_ok=True)
+    plt.savefig(out_path)
+
+
+
+
+
+
 def create_fine_tune_plot_averaged(baseline, test_set, methods, num_annotations_to_select_lst, num_dups):
     for num_annotations_to_select in num_annotations_to_select_lst:
         fine_tune_experiment.check_fine_tuning_models(baseline, test_set, num_dups, num_annotations_to_select)
@@ -157,6 +222,7 @@ def create_fine_tune_plot_averaged(baseline, test_set, methods, num_annotations_
         for j in range(len(num_annotations_to_select_lst)):
 
             dup_accuracies = []
+            dup_mean_abs_dics = []
             for dup_num in range(num_dups):
 
                 result_name = baseline["model_name"] + "_post_finetune_" + method + "_" + str(num_annotations_to_select_lst[j]) + "_annotations_dup_" + str(dup_num)
@@ -188,12 +254,15 @@ def create_fine_tune_plot_averaged(baseline, test_set, methods, num_annotations_
 
                 predicted_counts = [int((predictions[image_name]["scores"] > 0.5).size) for image_name in annotations.keys()]
                 annotated_counts = [int(annotations[image_name]["boxes"].shape[0]) for image_name in annotations.keys()]
+                mean_abs_dic = np.mean(abs(np.array(predicted_counts) - np.array(annotated_counts)))
 
                 test_set_accuracy = global_accuracy
 
                 # test_set_accuracy = global_accuracy #np.mean(accuracy)
                 # test_set_accuracy = np.mean(accuracies)
                 dup_accuracies.append(float(test_set_accuracy))
+
+                dup_mean_abs_dics.append(float(mean_abs_dic))
                 # results[method].append((num_fine_tuning_boxes, test_set_accuracy))
                 # results[method].append((num_fine_tuning_regions, test_set_accuracy))
 
@@ -203,8 +272,9 @@ def create_fine_tune_plot_averaged(baseline, test_set, methods, num_annotations_
                                     float(np.mean(dup_accuracies)), 
                                     float(np.std(dup_accuracies)),
                                     dup_accuracies,
-                                    annotated_counts,
-                                    predicted_counts))
+                                    float(np.mean(dup_mean_abs_dics)),
+                                    float(np.std(dup_mean_abs_dics)),
+                                    dup_mean_abs_dics))
 
     print(results)
 
